95cb75dc97437ee49648b9632e41463827da7154
JonathanLu1 <107448860+JonathanLu1@users.noreply.github.com>
2022-08-06 20:12:16 -0700

    Add files via upload

---
+++ Pelwett-Hackard-Analysis/Employee_Database_challenge.sql
@@ -0,0 +1,46 @@
+SELECT employees.emp_no, employees.first_name,
+    employees.last_name, titles.title, titles.from_date, titles.to_date
+INTO retirement_titles
+FROM employees
+INNER JOIN titles ON employees.emp_no = titles.emp_no
+WHERE(employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY employees.emp_no
+;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO more_titles
+FROM retirement_titles
+ORDER BY emp_no, title DESC;
+
+SELECT COUNT(more_titles.emp_no),
+more_titles.title
+INTO retiring
+FROM more_titles
+GROUP BY title
+ORDER BY COUNT(title)DESC;
+
+-- DELIVERABLE 2
+
+SELECT DISTINCT ON(employees.emp_no) employees.emp_no, 
+    employees.first_name, 
+    employees.last_name, 
+    employees.birth_date,
+    dept_emp.from_date,
+    dept_emp.to_date,
+    titles.title
+INTO mentorship_eligibilty
+FROM employees 
+Left outer Join dept_emp
+ON (employees.emp_no = dept_emp.emp_no)
+Left outer Join titles
+ON (employees.emp_no = titles.emp_no)
+WHERE (employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY employees.emp_no;
+
+SELECT * FROM mentorship_eligibilty;
